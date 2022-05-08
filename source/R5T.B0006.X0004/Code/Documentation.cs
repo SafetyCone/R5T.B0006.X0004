ae4b2c3678d0e84efdb4c16c19ae7d8e68d3bf16
davidcoats <david@getsafetycone.com>
2022-05-08 16:06:10 -0700

    Initial commit of source-stub.

---
+++ source/R5T.B0006.X0004/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.B0006.X0004
+{
+    /// <summary>
+    /// Generation, composite, extensions for ISyntaxGenerator, ISyntaxOperator, and related extensions.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
